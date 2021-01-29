07e2023c1ace1da2f037ed953d49b3b4c7404849
looplinux <amanre556@gmail.com>
2021-01-29 08:29:39 +0100

    update

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -34,7 +34,7 @@ pacman-key --populate archlinux
 
 
 
-groupscarli="adm,audio,disk,floppy,log,network,optical,rfkill,storage,video,wheel,sys"
+groupslooplinux="adm,audio,disk,floppy,log,network,optical,rfkill,storage,video,wheel,sys"
 useradd -m -g users -G $groupslooplinux -s /bin/bash liveuser
 passwd -d liveuser
 
